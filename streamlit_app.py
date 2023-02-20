d05d154c9416cf701610e32c11bc5f53f2d5f51b
motoneurona <125871886+motoneurona@users.noreply.github.com>
2023-02-20 20:32:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,16 @@ import altair as alt
 
 # Definir las preguntas y constructos
 preguntas_constr_1 = [
-    "How often have you felt sad or down in the dumps in the past two weeks?",
-    "How much have you been bothered by feeling hopeless or helpless in the past two weeks?",
-    "How often have you felt like a failure or that you have let yourself or your family down in the past two weeks?",
-    "How much have you been bothered by having little interest or pleasure in doing things in the past two weeks?",
-    "How often have you felt down on yourself or your life in the past two weeks?"
+    "Question 1- How often have you felt sad or down in the dumps in the past two weeks?",
+    "Question 2- How much have you been bothered by feeling hopeless or helpless in the past two weeks?",
+    "Question 3- How often have you felt like a failure or that you have let yourself or your family down in the past two weeks?",
+    "Question 4- How much have you been bothered by having little interest or pleasure in doing things in the past two weeks?",
+    "Question 5- How often have you felt down on yourself or your life in the past two weeks?"
 ]
 preguntas_constr_2 = [
-    "How motivated have you felt to make positive changes in your life in the past two weeks?",
-    "How often have you taken specific steps to make positive changes in your life in the past two weeks?",
-    "How much do you believe you can make positive changes in your life in the past two weeks?"
+    "Question 6- How motivated have you felt to make positive changes in your life in the past two weeks?",
+    "Question 7- How often have you taken specific steps to make positive changes in your life in the past two weeks?",
+    "Question 8- How much do you believe you can make positive changes in your life in the past two weeks?"
 ]
 
 # Crear la aplicación Streamlit
@@ -29,23 +29,55 @@ def main():
     st.title("Test to Assess for Depression")
     st.write("Instructions: Please carefully read each question and select the answer that best reflects your experience over the past two weeks. ")
 
+    st.markdown("<style>.css-1s80s3i {{margin-bottom: 20px;}}</style>", unsafe_allow_html=True)
+
+
     # Inicializar las sumas de los constructos
     suma_constr_1 = 0
     suma_constr_2 = 0
-
+    
+    # Define the mapping from text options to numerical values
+    option_values = {
+    'Not at all': -3,
+    'Several days a week': -2,
+    'Half the days': -1,
+    'Most days': 1,
+    'Every day': 2,
+    'All the time': 3
+    }
+    
     # Crear un loop para iterar a través de las preguntas y respuestas de cada constructo
     for i, pregunta in enumerate(preguntas_constr_1):
-        respuesta = st.slider(pregunta, -3, 3, value = 0)
-        suma_constr_1 += respuesta
-    suma_constr_1 = suma_constr_1 * -1
+        
+        # Use markdown to display the question in a bigger font
+        st.markdown("<p style='font-size: 20px'>{}</p>".format(pregunta), unsafe_allow_html=True)
+        
+        # Use select_slider with text options, and then convert the response to a number using the mapping
+        respuesta = st.select_slider(" ", options=list(option_values.keys()), key=f"constr_1_{i}")
+        respuesta_num = option_values[respuesta]
+
+        # Add margin to the select_slider element
+        st.markdown("<style>.css-1s80s3i {{margin-bottom: 20px;}}</style>", unsafe_allow_html=True)
+
+        suma_constr_1 += respuesta_num
+        suma_constr_1 = suma_constr_1 * -1
 
     for i, pregunta in enumerate(preguntas_constr_2):
-        respuesta = st.slider(pregunta, -3, 3, value = 0)
-        suma_constr_2 += respuesta
+        st.markdown("<p style='font-size: 20px'>{}</p>".format(pregunta), unsafe_allow_html=True)
+                
+        respuesta = st.select_slider(" ", options=list(option_values.keys()), key=f"constr_2_{i}")
+        respuesta_num = option_values[respuesta]
+        
+        # Add margin to the select_slider element
+        st.markdown("<style>.css-1s80s3i {{margin-bottom: 20px;}}</style>", unsafe_allow_html=True)
+
+        suma_constr_2 += respuesta_num
+        suma_constr_2 = suma_constr_2
+        
     if st.button("Submit"):
         # Mostrar las sumas de cada constructo
-        st.write("Depressive symptons: ", suma_constr_1)
-        st.write("Motivation to change: ", suma_constr_2)
+        # st.write("Depressive symptons: ", suma_constr_1)
+        # st.write("Motivation to change: ", suma_constr_2)
     
         # Crear un dataframe con las sumas de cada constructo
         datos = pd.DataFrame({
@@ -58,8 +90,8 @@ def main():
             size=100,
             color='red'
         ).encode(
-        x=alt.X('Depressive symptons', axis=alt.Axis(titleFontSize=18, labelFontSize=16, values=[0], ticks=True, grid=True), scale=alt.Scale(domain=(-25, 25))),
-        y=alt.Y('Motivation to change', axis=alt.Axis(titleFontSize=18, labelFontSize=16, values=[0], ticks=True, grid=True), scale=alt.Scale(domain=(-25, 25)))
+        x=alt.X('Depressive symptons', axis=alt.Axis(titleFontSize=18, labelFontSize=16, values=[0], ticks=True, grid=True), scale=alt.Scale(domain=(-10, 10))),
+        y=alt.Y('Motivation to change', axis=alt.Axis(titleFontSize=18, labelFontSize=16, values=[0], ticks=True, grid=True), scale=alt.Scale(domain=(-10, 10)))
         ).properties(
             width=600,
             height=500,
